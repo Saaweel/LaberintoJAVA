e8864881741e2bd1f15a7ea1b56ab2d61a683049
Saaweel <sawelsports@gmail.com>
2023-05-08 19:54:23 +0200

    .

---
+++ src/main/java/com/saaweel/Direcciones.java
@@ -0,0 +1,5 @@
+package com.saaweel;
+
+public enum Direcciones {
+    N, S, E, O;
+}
---
+++ src/main/java/com/saaweel/Harry.java
@@ -0,0 +1,8 @@
+package com.saaweel;
+
+import java.util.LinkedList;
+
+public class Harry {
+    private int position;
+    private LinkedList<Direcciones> path;
+}
