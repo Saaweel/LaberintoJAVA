da49e8ed610acb45c5e40e853dd306357200a39e
Saaweel <sawelsports@gmail.com>
2023-05-12 16:52:11 +0200

    .

--- src/main/java/com/saaweel/Harry.java
+++ src/main/java/com/saaweel/Harry.java
@@ -5,4 +5,111 @@ import java.util.LinkedList;
 public class Harry {
     private int position;
     private LinkedList<Direcciones> path;
+    private LinkedList<Integer> visited;
+
+    public Harry(int position) {
+        this.position = position;
+        this.path = new LinkedList<>();
+        this.visited = new LinkedList<>();
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public LinkedList<Direcciones> getPath() {
+        return path;
+    }
+
+    public LinkedList<Integer> getVisited() {
+        return visited;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setPath(LinkedList<Direcciones> path) {
+        this.path = path;
+    }
+
+    public void setVisited(LinkedList<Integer> visited) {
+        this.visited = visited;
+    }
+
+    public void addVisited(int position) {
+        this.visited.add(position);
+    }
+
+    public void addPath(Direcciones direction) {
+        this.path.add(direction);
+    }
+
+    public void removeVisited(int position) {
+        this.visited.remove(position);
+    }
+
+    public void removePath(Direcciones direction) {
+        this.path.remove(direction);
+    }
+
+    public boolean isVisited(int position) {
+        return this.visited.contains(position);
+    }
+
+    public boolean traceRoute(Mapa m) {
+        if (this.position == m.getExit()) {
+            this.position = 0;
+            return true;
+        } else {
+            if (!m.isLastFloor(this.position) && !m.existWall(this.position, this.position + m.getAncho()) && !isVisited(this.position + m.getAncho())) {
+                this.position += m.getAncho();
+                addVisited(this.position);
+                addPath(Direcciones.S);
+                if (traceRoute(m)) {
+                    return true;
+                } else {
+                    removePath(Direcciones.S);
+                    this.position -= m.getAncho();
+                }
+            }
+
+            if (!m.isLastWall(this.position) && !m.existWall(this.position, this.position + 1) && !isVisited(this.position + 1)) {
+                this.position += 1;
+                addVisited(this.position);
+                addPath(Direcciones.E);
+                if (traceRoute(m)) {
+                    return true;
+                } else {
+                    removePath(Direcciones.E);
+                    this.position -= 1;
+                }
+            }
+
+            if (!m.isFirstWall(this.position) && !m.existWall(this.position, this.position - 1) && !isVisited(this.position - 1)) {
+                this.position -= 1;
+                addVisited(this.position);
+                addPath(Direcciones.O);
+                if (traceRoute(m)) {
+                    return true;
+                } else {
+                    removePath(Direcciones.O);
+                    this.position += 1;
+                }
+            }
+
+            if (!m.isFirstFloor(this.position) && !m.existWall(this.position, this.position - m.getAncho()) && !isVisited(this.position - m.getAncho())) {
+                this.position -= m.getAncho();
+                addVisited(this.position);
+                addPath(Direcciones.N);
+                if (traceRoute(m)) {
+                    return true;
+                } else {
+                    removePath(Direcciones.N);
+                    this.position += m.getAncho();
+                }
+            }
+            return false;
+        }
+    }
 }
--- src/main/java/com/saaweel/Mapa.java
+++ src/main/java/com/saaweel/Mapa.java
@@ -6,11 +6,13 @@ public class Mapa {
     private int ancho;
     private int alto;
     private LinkedList<Pared> paredes;
+    private Harry harry;
 
     public Mapa(int alto, int ancho) {
         this.ancho = ancho;
         this.alto = alto;
         this.paredes = new LinkedList<Pared>();
+        this.harry = new Harry(0);
     }
 
     public int getAncho() {
@@ -36,13 +38,13 @@ public class Mapa {
             y = aux;
         }
 
-        if (x >= 0 && x < this.ancho * this.alto && y >= 0 && y < this.ancho * this.alto && !existePared(x, y)) {
+        if (x >= 0 && x < this.ancho * this.alto && y >= 0 && y < this.ancho * this.alto && !existWall(x, y)) {
             Pared pared = new Pared(x, y);
             this.paredes.add(pared);
         }
     }
 
-    public boolean existePared(int x, int y) {
+    public boolean existWall(int x, int y) {
         if (x > y) {
             int aux = x;
             x = y;
@@ -55,6 +57,26 @@ public class Mapa {
             }
         }
         return false;
+    }
+
+    public int getExit() {
+        return this.ancho * this.alto - 1;
+    }
+
+    public boolean isFirstFloor(int position) {
+        return position < this.ancho;
+    }
+
+    public boolean isLastFloor(int position) {
+        return position >= this.ancho * (this.alto - 1);
+    }
+
+    public boolean isFirstWall(int position) {
+        return position % this.ancho == 0;
+    }
+
+    public boolean isLastWall(int position) {
+        return position % this.ancho == this.ancho - 1;
     }
 
     @Override
@@ -73,18 +95,22 @@ public class Mapa {
             for (int j = 0; j < this.ancho; j++) {
                 int pos = i * this.ancho + j;
 
-                if (i == this.alto - 1) {
-                    mapa += "_";
+                if (pos == this.harry.getPosition()) {
+                    mapa += "H";
                 } else {
-                    if (existePared(pos, pos + this.ancho)) {
+                    if (i == this.alto - 1) {
                         mapa += "_";
                     } else {
-                        mapa += " ";
+                        if (existWall(pos, pos + this.ancho)) {
+                            mapa += "_";
+                        } else {
+                            mapa += " ";
+                        }
                     }
                 }
-
+    
                 if (j != this.ancho - 1) {
-                    if (existePared(pos, pos + 1)) {
+                    if (existWall(pos, pos + 1)) {
                         mapa += "|";
                     } else {
                         mapa += " ";
